61466f9707291d6214e3bf2534afcff6d4ecabe8
Fernando Brûlé <fernandobrule@gmail.com>
2020-07-08 15:54:17 -0300

    update deps

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -9,7 +9,7 @@ object Dependencies {
   lazy val mysql8JavaClient = "mysql" % "mysql-connector-java" % "8.0.18" % "test"
 
 
-  lazy val fragnosticSupport = "com.fragnostic" % "fragnostic-support_2.13" % "0.1.14-SNAPSHOT" % "test"
-  lazy val fragnosticConfEnv = "com.fragnostic" % "fragnostic-conf-env_2.13" % "0.1.0-SNAPSHOT" % "test"
+  lazy val fragnosticSupport = "com.fragnostic" % "fragnostic-support_2.13" % "0.1.14" % "test"
+  lazy val fragnosticConfEnv = "com.fragnostic" % "fragnostic-conf-env_2.13" % "0.1.0" % "test"
 
 }
