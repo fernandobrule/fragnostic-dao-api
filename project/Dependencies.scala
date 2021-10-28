fc5e97ea96656ed5d46f06633f0a25394d8139ef
Fernando Brûlé <fernandobrule@gmail.com>
2021-10-28 20:50:33 -0300

    upd deps

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,7 +4,7 @@ import Keys._
 object Dependencies {
 
   lazy val fragnosticSupport = "com.fragnostic" % "fragnostic-support_2.13" % "0.1.17" % "test"
-  lazy val fragnosticConfEnv = "com.fragnostic" % "fragnostic-conf-env_2.13" % "0.1.7" % "test"
+  lazy val fragnosticConfEnv = "com.fragnostic" % "fragnostic-conf-env_2.13" % "0.1.8" % "test"
 
   lazy val logbackClassic      = "ch.qos.logback" % "logback-classic"          % "1.2.3" % "provided"
   lazy val slf4jApi            = "org.slf4j"      % "slf4j-api"                % "1.7.25" % "provided"
