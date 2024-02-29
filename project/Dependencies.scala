525ddb9e868d30dc7c2686ee4c17766d83e0c629
Fernando Brûlé <fernandobrule@gmail.com>
2024-02-29 17:24:30 -0300

    update sbt conf, dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object Dependencies {
 
-  lazy val fragnosticSupport          = "com.fragnostic"        % "fragnostic-support_2.13"        % "0.1.19-SNAPSHOT" % "test"
+  lazy val fragnosticSupport          = "com.fragnostic"        % "fragnostic-support_2.13"        % "0.1.19" % "test"
   lazy val fragnosticConfEnv          = "com.fragnostic"        %  "fragnostic-conf-env_2.13"      % "0.1.10-SNAPSHOT"
 
   lazy val logbackClassic             = "ch.qos.logback"        % "logback-classic"                % "1.3.0-alpha16" % "runtime"
