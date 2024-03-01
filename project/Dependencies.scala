1393b4fd625ac3d0e73d25637f3197d9be6a3a70
Fernando Brûlé <fernandobrule@gmail.com>
2024-03-01 19:09:30 -0300

    Update dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object Dependencies {
 
-  lazy val fragnosticConfEnv       = "com.fragnostic"        %  "fragnostic-conf-env_2.13" % "0.1.11"
+  lazy val fragnosticConfEnv       = "com.fragnostic"        %  "fragnostic-conf-env_2.13" % "0.1.12"
   lazy val fragnosticSupport       = "com.fragnostic"        % "fragnostic-support_2.13"   % "0.1.19" % "test"
   lazy val logbackClassic          = "ch.qos.logback"        % "logback-classic"           % "1.5.0" % "runtime"
   lazy val mysql8JavaClient        = "com.mysql"             % "mysql-connector-j"         % "8.3.0"
